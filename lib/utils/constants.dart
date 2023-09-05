96443b7178b502338ce3bf7d14ea210d77c3efd3
Anis Saadouli <anissaadouli8@gmail.com>
2023-09-05 23:15:10 +0100

    modification

--- lib/utils/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-// Colors
-const kBackgroundColor = Color.fromARGB(255, 153, 196, 185);
-const kPrimaryColor = Color(0xFF2D5D70);
-const kSecondaryColor = Color.fromARGB(255, 1, 21, 21);
-const Color kInputFieldFillColor = Colors.white;
-
